2dee5a988f5c4688fd99a1ba474393aa3b079bd1
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-11-16 03:49:50 +0700

    :zap:fix bug time convert

---
+++ src/routers/timeconvert.ts
@@ -0,0 +1,17 @@
+export default function timeconvert(due_date: string) {
+
+    // const due_date = "November 28, 2022 at 23:00";
+    const due_date_regex = /(\w+) (\d+), (\d+) at (\d+):(\d+)/
+
+    const [_, month, day, year, hour, minute] = due_date.match(due_date_regex) || []
+
+    const now = new Date()
+    const due_date_date = new Date(`${month} ${day}, ${year} ${hour}:${minute}`)
+    const due_date_date_unix = due_date_date.getTime() / 1000
+
+    const now_unix = now.getTime() / 1000
+    const diff = due_date_date_unix - now_unix
+
+
+    return diff
+}
